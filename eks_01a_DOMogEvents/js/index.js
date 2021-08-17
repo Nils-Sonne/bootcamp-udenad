ee5d760e7e3ee60357ed6c37502a5ff22d163db6
Nils-Sonne <U2407197@RTS-365.DK>
2021-08-17 13:52:57 +0200

    Add assignment 01A

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -2,6 +2,33 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+
+
+	let decreaseBtn = document.querySelector("#btn-count-decrease");
+	let increaseBtn = document.querySelector("#btn-count-increase");
+	let tekstcountelement = document.querySelector("#text-count");
+	let count = 5;
+
+	// Function count(){
+	// 	tekstcountelement.innerHTML = count;
+	// }
+
+	decreaseBtn.addEventListener("click", () => {
+		if (count>0){
+		count--;
+		tekstcountelement.innerHTML = count;
+		};
+	
+	  });
+
+
+	  increaseBtn.addEventListener("click", () => {
+		
+		if (count<10){
+		count++;
+		tekstcountelement.innerHTML = count;
+		};
+
+	  });
 
 }); // Afslutter: DOMContentLoaded
