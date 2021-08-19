f9dd3286e3c19fb228b5b9b201dcf8cde4799f7b
Nils-Sonne <U2407197@RTS-365.DK>
2021-08-19 10:02:57 +0200

    Add assignment 01C

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -2,18 +2,12 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-
-
-	let decreaseBtn = document.querySelector("#btn-count-decrease");
 	let increaseBtn = document.querySelector("#btn-count-increase");
+	let decreaseBtn = document.querySelector("#btn-count-decrease");
 	let textCountElement = document.querySelector("#text-count");
 	let number = 5;
-	let min = 0;
-	let max = 15;
-
-
-
-
+	let minValue = 0;
+	let maxValue = 15;
 
 	 function updateNumber(){
 		textCountElement.textContent = number;
@@ -21,23 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	 updateNumber();
 
-	decreaseBtn.addEventListener("click", () => {
-		if (number> min){
-		number--;
-		updateNumber ();
+	 function checkMinMaxValue(direction){
+		 if (direction === "inc" && number < maxValue) {
+			 number++;
+			 updateNumber();
+		 }
+
+		 if (direction === "dec" && number > minValue) {
+			number--;
+			updateNumber();
 		}
+	}
+	 
 	
-	  });
-
-
-	  increaseBtn.addEventListener("click", () => {
-		
-		if (number< max){
-		number++;
-		updateNumber ();
-		}
-
-	  });
+	increaseBtn.addEventListener("click", () => checkMinMaxValue ("inc"));
+	decreaseBtn.addEventListener("click", () => checkMinMaxValue ("dec"));  // => er det samme som at skrive function 
 
 }); // Afslutter: DOMContentLoaded
 //done
