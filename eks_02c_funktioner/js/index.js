11547a63a9b32a620617bc40bf0b06b695887704
Nils-Sonne <U2407197@RTS-365.DK>
2021-08-19 12:39:29 +0200

    Add assignment 02C

--- eks_02c_funktioner/js/index.js
+++ eks_02c_funktioner/js/index.js
@@ -1,6 +1,14 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
 
-}); // Afslutter: DOMContentLoaded
+	function hils (navn){ 
+		return "hej med dig " + navn;       //declaring	
+	}
+	var hilsenFraOle = hils ("Ole");
+	console.log(hilsenFraOle);
+
+
+}); 
+
+// Afslutter: DOMContentLoaded
