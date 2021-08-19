2b3699fd5f14789d7990b882a2fb2bc3480badc4
Nils-Sonne <U2407197@RTS-365.DK>
2021-08-19 15:08:36 +0200

    Add Assignment 03A

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,15 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+
+	const names = ['Simon', 'Joachim', 'Kristoffer',];
+    const list = document.getElementById("liste");
+
+	names.forEach((item) => {
+		let li = document.createElement("li");
+		li.innerHTML = item;
+		list.appendChild(li);
+	  })
+
 
 }); // Afslutter: DOMContentLoaded
